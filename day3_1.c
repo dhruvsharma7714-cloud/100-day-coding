3d0deb5c27c4d4b871c32ec3d7e2b2165a1b4763
dhruv sharma <dhruv.sharma7714@gmail.com>
2026-08-12 22:12:43 +0530

    Add files via upload

---
+++ day3_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a , b , c;
+    printf("enter the first number:"  );
+    scanf("%d" , &a);
+    printf("enter the second number:"  );
+    scanf("%d" , &b);
+     
+    c = a;
+    a = b;
+    b = c;
+   
+    printf("after swap: %d %d", a , b);
+    return 0;
+    
+    
+}
