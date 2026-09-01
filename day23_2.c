d5fd6e88edb27f24bc6b0a8d8b2120ce346d0dbf
dhruv sharma <dhruv.sharma7714@gmail.com>
2026-09-01 23:08:18 +0530

    Add files via upload

---
+++ day23_2.c
@@ -0,0 +1,29 @@
+/*Q46: Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+*****
+*****
+*****
+*****
+
+*/
+#include <stdio.h>
+int main()
+{
+    printf("*****\n");
+    printf("*****\n");
+    printf("*****\n");
+    printf("*****\n");
+    printf("*****\n");
+    return 0;
+}
