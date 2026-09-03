2dc3d1e62553286dfae97c80cad1fd6a18130329
dhruv sharma <dhruv.sharma7714@gmail.com>
2026-09-03 23:12:06 +0530

    Add files via upload

---
+++ day25.c
@@ -0,0 +1,25 @@
+/*Q49: Write a program to print the following pattern:
+5
+45
+345
+2345
+12345
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n = 5, i, j;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = n - i + 1; j <= n; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
