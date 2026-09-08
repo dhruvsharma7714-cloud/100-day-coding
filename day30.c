253e310ff12531d33c4ba5639b3204910e655e2d
dhruv sharma <dhruv.sharma7714@gmail.com>
2026-09-08 22:14:27 +0530

    Add files via upload

---
+++ day30.c
@@ -0,0 +1,39 @@
+/*Q60: Count positive, negative, and zero elements in an array.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+-1 0 1 2 -2
+Output 1:
+Positive=2, Negative=2, Zero=1
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i;
+    int pos = 0, neg = 0, zero = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements: ");
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] > 0)
+            pos++;
+        else if (arr[i] < 0)
+            neg++;
+        else
+            zero++;
+    }
+
+    printf("Positive=%d, Negative=%d, Zero=%d\n", pos, neg, zero);
+
+    return 0;
+}
